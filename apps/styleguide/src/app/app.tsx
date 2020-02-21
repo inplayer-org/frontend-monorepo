5255e88a36c511a65663f00bef15b246661a2097
srdjanRakic <srgjan.rakic@gmail.com>
2020-02-21 13:22:53 +0100

    Initial commit

---
+++ apps/styleguide/src/app/app.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { ThemeWrapper } from '@inplayer-org/inplayer-ui';
+import { ThemeProvider } from 'styled-components';
+import SidebarMenu from './components/SidebarMenu';
+import styled from 'styled-components';
+import '@inplayer-org/ui/paywall-previews';
+
+const Wrapper = styled.div`
+  display: flex;
+`;
+
+const Root: React.FC = () => (
+  <ThemeWrapper withProvider={ThemeProvider}>
+    <Wrapper>
+      <SidebarMenu />
+    </Wrapper>
+  </ThemeWrapper>
+);
+
+export default Root;
