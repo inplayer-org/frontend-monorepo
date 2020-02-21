6943bbfdfee243b406c3f1ffed41dbf8c06b042a
srdjanRakic <srgjan.rakic@gmail.com>
2020-02-21 14:26:19 +0100

    Fix preview default exports

--- libs/paywall-previews/src/lib/paywall-previews.ts
+++ libs/paywall-previews/src/lib/paywall-previews.ts
@@ -1,7 +1,10 @@
-import PreviewTemplatesWrapper from './components/PreviewTemplatesWrapper';
-import PaywallPreviews from './PreviewTemplates';
+export { default as Preview1 } from './PreviewTemplates/Preview1';
+export { default as Preview2 } from './PreviewTemplates/Preview2';
+export { default as Preview3 } from './PreviewTemplates/Preview3';
+export { default as Preview4 } from './PreviewTemplates/Preview4';
+export { default as Preview5 } from './PreviewTemplates/Preview5';
+export { default as Preview6 } from './PreviewTemplates/Preview6';
+export { default as Preview7 } from './PreviewTemplates/Preview7';
 
-export {
-  PreviewTemplatesWrapper,
-  PaywallPreviews
-};
+
+
--- libs/paywall-previews/src/lib/utils/generateComponentData.ts
+++
@@ -1,7 +0,0 @@
-import previewTemplates from '../PreviewTemplates'
-
-type PreviewTemplate = keyof typeof previewTemplates;
-
-export const getPreviewTemplate = (preview: PreviewTemplate) => {
-    return previewTemplates[preview] || {};
-}
